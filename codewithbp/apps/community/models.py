b5b299f648249844d724b79e8444d9b309404f77
jonzlin95 <jonzlin95@gmail.com>
2014-02-06 20:39:51 -0800

    #1 created models

--- codewithbp/apps/community/models.py
+++ codewithbp/apps/community/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from codewithbp.apps.article.models import Article
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    favorites = models.ManyToManyField(Article)
--- codewithbp/apps/community/views.py
+++ codewithbp/apps/community/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+class Community(models.Model):
+    name = models.CharField(max_length=25)
